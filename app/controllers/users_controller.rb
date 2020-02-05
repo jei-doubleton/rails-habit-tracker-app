2d10290d30b4f4b65edefb4cf24be4885e1c278f
jei-doubleton <jennifer.singleton@gmail.com>
2020-02-04 21:03:55 -0600

    add user login flow to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,17 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.create(user_params)
+        return redirect_to '/signup' unless @user.save
+        
+        session[:user_id] = @user.id 
+        
+        redirect_to '/'
     end
-    
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+
 end
