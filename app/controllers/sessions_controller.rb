3aa6856c658e04f2ad1a2ebf9054942494d1a708
jei-doubleton <jennifer.singleton@gmail.com>
2020-02-05 20:30:34 -0600

    add login form and create session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,18 @@
 class SessionsController < ApplicationController
 
     def new
+        @user = User.new
     end
 
     def create
+        user = User.find_by(id: params[:user][:name])
+        user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to 'welcome#index'
     end
     
+    private
+    def session_params
+        params.require(:user).permit(:name, :password)
+    end
 end
