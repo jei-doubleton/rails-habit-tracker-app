d0ed967c4ba94de961817d40ed1f976b49e6fd94
jei-doubleton <jennifer.singleton@gmail.com>
2020-02-10 20:20:11 -0600

    add navbar and helper methods for current_user and invalid_user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def current_user 
+        @user = User.find(session[:user_id]) unless invalid_user
+      end
+    
+    def invalid_user
+        redirect_to '/login' if User.find(session[:user_id]) == nil
+    end
+
 end
