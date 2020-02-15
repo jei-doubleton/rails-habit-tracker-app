62ee388b710fb6fd7db156b97ae9426d98b81c31
jei-doubleton <jennifer.singleton@gmail.com>
2020-02-15 13:40:58 -0600

    add message to logged in users trying to access login; add syling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,21 @@
 class SessionsController < ApplicationController
 
     def new
+        if logged_in?
+            flash[:notice] = "You're already logged in!"
+            redirect_to user_path(current_user)
+        end
+
         @user = User.new
     end
 
     def create
-        user = User.find_by(name: params[:user][:name])
-        user.authenticate(params[:password])
+        @user = User.find_by(name: params[:user][:name])
+        @user.authenticate(params[:password])
 
-        session[:user_id] = user.id
+        session[:user_id] = @user.id
         
-        redirect_to "/users/#{user.id}"
+        redirect_to "/users/#{@user.id}"
     end
 
     def destroy
