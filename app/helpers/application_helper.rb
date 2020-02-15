7b3d4becc5067ea8af54f9426cff66d6a7057a30
jei-doubleton <jennifer.singleton@gmail.com>
2020-02-15 12:39:33 -0600

    work on layout and css for title and navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,13 @@
 module ApplicationHelper
 
     def current_user 
-        @current_user ||= User.find(session[:user_id])
+        if session[:user_id]
+            @user ||= User.find(session[:user_id])
+        end
     end
 
     def logged_in?
         current_user ? true : false
     end
-    
+
 end
