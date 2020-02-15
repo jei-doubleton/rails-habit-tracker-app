2bdf1fd62c5c2f5a247c185c6f57c3cfc5363711
jei-doubleton <jennifer.singleton@gmail.com>
2020-02-15 13:14:00 -0600

    update user show page to show user's habits

--- app/models/my_habit.rb
+++ app/models/my_habit.rb
@@ -1,4 +1,8 @@
 class MyHabit < ApplicationRecord
     belongs_to :user 
     belongs_to :habit 
+
+    def name
+        self.habit.name
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
     has_many :habits, through: :my_habits
     has_secure_password
     validates :name, presence: { message: "Whoops, you need to include a username!"}, uniqueness: { message: "Sorry, that username is taken. Try another one."}
+
 end
