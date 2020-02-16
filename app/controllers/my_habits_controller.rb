70f6db94871c58b3b3969609ad6e76873fb5629d
jei-doubleton <jennifer.singleton@gmail.com>
2020-02-16 14:34:17 -0600

    add params method to my_habits controller

--- app/controllers/my_habits_controller.rb
+++ app/controllers/my_habits_controller.rb
@@ -3,5 +3,14 @@ class MyHabitsController < ApplicationController
 
     def new
     end
+
+    def create
+
+    end
+
+    private
+    def my_habit_params
+        params.require(:my_habit).permit(:habit_id, :frequency, :notes, :status, habit_attributes: [:name])
+    end
     
 end
