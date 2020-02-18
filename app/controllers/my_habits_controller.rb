78322ba4a952d8e7d1e6c5f0f6cd7ea2eda24a63
jei-doubleton <jennifer.singleton@gmail.com>
2020-02-17 20:55:10 -0600

    switch to datalist autocomplete for habit

--- app/controllers/my_habits_controller.rb
+++ app/controllers/my_habits_controller.rb
@@ -3,16 +3,18 @@ class MyHabitsController < ApplicationController
 
     def new
         @my_habit = MyHabit.new
+        @habit = @my_habit.build_habit
     end
 
     def create
         @my_habit = MyHabit.new(my_habit_params)
+        @my_habit.user_id = current_user.id
         raise @my_habit.inspect
     end
 
     private
     def my_habit_params
-        params.require(:my_habit).permit(:habit_id, :frequency, :notes, :status, habit_attributes: [:name])
+        params.require(:my_habit).permit(:habit_name, :frequency, :notes, :status)
     end
     
 end
--- app/models/my_habit.rb
+++ app/models/my_habit.rb
@@ -1,9 +1,17 @@
 class MyHabit < ApplicationRecord
     belongs_to :user 
     belongs_to :habit 
-    accepts_nested_attributes_for :habit
 
     def name
         self.habit.name
     end
+
+    def habit_name=(name)
+        self.habit = Habit.find_or_create_by(name: name)
+    end
+
+    def habit_name
+        self.habit ? self.habit.name : nil
+    end
+
 end
