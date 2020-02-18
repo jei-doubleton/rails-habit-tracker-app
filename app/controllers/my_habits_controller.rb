726a47eacccca4227a9c1525dcd272f5a2d84c3d
jei-doubleton <jennifer.singleton@gmail.com>
2020-02-17 19:58:09 -0600

    start working on creating my_habit in my_habit controller

--- app/controllers/my_habits_controller.rb
+++ app/controllers/my_habits_controller.rb
@@ -2,10 +2,12 @@ class MyHabitsController < ApplicationController
     before_action :authentication_required
 
     def new
+        @my_habit = MyHabit.new
     end
 
     def create
-
+        @my_habit = MyHabit.new(my_habit_params)
+        raise @my_habit.inspect
     end
 
     private
--- app/models/my_habit.rb
+++ app/models/my_habit.rb
@@ -1,7 +1,7 @@
 class MyHabit < ApplicationRecord
     belongs_to :user 
     belongs_to :habit 
-    accepts_nested_attributes_for: :habits
+    accepts_nested_attributes_for :habit
 
     def name
         self.habit.name
